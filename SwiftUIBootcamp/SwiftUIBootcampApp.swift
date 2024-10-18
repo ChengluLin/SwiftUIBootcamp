cd1492231f9a83dff7bdbf26591e0dee7f9dea4c
Kevin <chenglu7701@gmail.com>
2024-10-18 17:33:08 +0800

    first commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by ChengLu on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
