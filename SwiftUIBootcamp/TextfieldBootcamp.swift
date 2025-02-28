f67a0b718293624f71b104fc160419b49145e5e2
Kevin <chenglu7701@gmail.com>
2025-02-28 12:55:25 +0800

    TextfieldBootcamp update

--- SwiftUIBootcamp/TextfieldBootcamp.swift
+++ SwiftUIBootcamp/TextfieldBootcamp.swift
@@ -10,11 +10,45 @@ import SwiftUI
 struct TextfieldBootcamp: View {
     
     @State var textFieldText: String = ""
+    @State var dataArray: [String] = []
     
     var body: some View {
-        TextField("Type something here...", text: $textFieldText)
-//            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .background(Color.gray)
+        
+        NavigationView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                //            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .background(Color.gray.opacity(0.3).cornerRadius(10))
+                    .foregroundStyle(.red)
+                    .font(.headline)
+                Button(action: {
+                    saveText()
+                }, label: {
+                    Text("Save".uppercased())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue.cornerRadius(10))
+                        .foregroundColor(.white)
+                        .font(.headline)
+                })
+                
+                ForEach(dataArray, id: \.self) { data in
+                    Text(data)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("TextField Bootcamp!")
+        }
+       
+        
+    }
+    
+    func saveText() {
+        dataArray.append(textFieldText)
+        textFieldText = ""
     }
 }
 
