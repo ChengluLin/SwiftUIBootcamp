a16dce03990789b1d5b2b8eafc601f076c46cf77
Kevin <chenglu7701@gmail.com>
2025-03-11 12:46:43 +0800

    TextEditorBootcamp finish

--- SwiftUIBootcamp/TextEditorBootcamp.swift
+++ SwiftUIBootcamp/TextEditorBootcamp.swift
@@ -9,23 +9,35 @@ import SwiftUI
 
 struct TextEditorBootcamp: View {
     
-    @State var textEditorText: String = ""
+    @State var textEditorText: String = "This is the starting text."
+    @State var savedText: String = ""
     
     var body: some View {
         NavigationView {
             VStack {
                 TextEditor(text: $textEditorText)
+                    .frame(height: 250)
+//                    .background(Color.red)
+                    .colorMultiply(Color.gray)
+                    .cornerRadius(10)
                 Button(action: {
-                    
+                    savedText = textEditorText
                 }, label: {
                     Text("Save".uppercased())
                         .font(.headline)
+                        .foregroundStyle(.white)
                         .padding()
                         .frame(maxWidth: .infinity)
                         .background(Color.blue)
                         .cornerRadius(10)
                 })
+                Text(savedText)
+                
+                Spacer()
             }
+            .padding()
+            .background(Color.green)
+            .navigationTitle("TextEditor Bootcamp!")
         }
     }
 }
