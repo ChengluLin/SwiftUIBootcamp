23a8349ae1da4aba95e5521adb757dad7b2c0543
Kevin <chenglu7701@gmail.com>
2025-04-01 12:59:15 +0800

    ColorPickerBootcamp

---
+++ SwiftUIBootcamp/ColorPickerBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPickerBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by ChengLu on 2025/4/1.
+//
+
+import SwiftUI
+
+struct ColorPickerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorPickerBootcamp()
+}
