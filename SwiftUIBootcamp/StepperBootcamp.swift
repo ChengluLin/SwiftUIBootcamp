3eee744828a05fc0295cb49796f5261d6f1933b8
Kevin <chenglu7701@gmail.com>
2025-04-29 13:15:34 +0800

    StepperBootcamp

--- SwiftUIBootcamp/StepperBootcamp.swift
+++ SwiftUIBootcamp/StepperBootcamp.swift
@@ -10,10 +10,32 @@ import SwiftUI
 struct StepperBootcamp: View {
     
     @State var stepperValue: Int = 10
+    @State var withIncrement: CGFloat = 0
     
     var body: some View {
-        Stepper("Stepper: \(stepperValue)", value: $stepperValue)
+        VStack {
+            Stepper("Stepper: \(stepperValue)", value: $stepperValue)
+                .padding(30)
+            
+            RoundedRectangle(cornerRadius: 25.0)
+                .frame(width: 100 + withIncrement, height: 100)
+            
+            Stepper("Stepper 2") {
+                // increment
+                incrementWith(amount: 30)
+            } onDecrement: {
+                // decrement
+                incrementWith(amount: -30)
+
+            }
             .padding(30)
+        }
+    }
+    
+    func incrementWith(amount: CGFloat) {
+        withAnimation(.easeInOut) {
+            withIncrement += amount
+        }
     }
 }
 
