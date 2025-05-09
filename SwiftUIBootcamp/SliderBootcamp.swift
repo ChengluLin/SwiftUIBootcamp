2081176db7783768a88bd6fed4b71c34e06ac222
Kevin <chenglu7701@gmail.com>
2025-05-09 15:14:33 +0800

    SliderBootcamp

--- SwiftUIBootcamp/SliderBootcamp.swift
+++ SwiftUIBootcamp/SliderBootcamp.swift
@@ -10,14 +10,35 @@ import SwiftUI
 struct SliderBootcamp: View {
     
     @State var sliderValue: Double = 3
+    @State var color: Color = .red
     
     var body: some View {
         VStack {
             Text("Rating")
-            Text("\(sliderValue)")
-//            Slider(value: $sliderValue, in: 1...5)
-            Slider(value: $sliderValue, in: 1...5, step: 1.0)
-                .accentColor(.red)
+            Text(
+                String(format: "%.1f", sliderValue)
+            )
+            .foregroundStyle(color)
+            //            Text("\(sliderValue)")
+            //            Slider(value: $sliderValue, in: 1...5)
+            //            Slider(value: $sliderValue, in: 1...5, step: 1.0)
+            Slider(
+                value: $sliderValue,
+                in: 1...5,
+                step: 1.0,
+                onEditingChanged: { (_) in
+                    color = .green
+                },
+                minimumValueLabel:
+                    Text("1")
+                    .font(.largeTitle)
+                    .foregroundStyle(.orange)
+                ,
+                maximumValueLabel: Text("5"),
+                label: {
+                    Text("Title")
+                })
+            .accentColor(.red)
         }
     }
 }
