4391772d29d2bb7d1efd8f983832b275feff4114
Kevin <chenglu7701@gmail.com>
2025-05-16 16:20:37 +0800

    TabViewBootcamp

--- SwiftUIBootcamp/TabViewBootcamp.swift
+++ SwiftUIBootcamp/TabViewBootcamp.swift
@@ -9,18 +9,41 @@ import SwiftUI
 
 struct TabViewBootcamp: View {
     var body: some View {
+        TabView{
         TabView {
-            Text("HOME TAB")
+            HomeView()
                 .tabItem {
                     Image(systemName: "house.fill")
                     Text("Home")
                 }
-            Text("BROWSE TAB")
-                .tabItem(<#T##label: () -> View##() -> View#>)
+            
+                Text("BROWSE TAB")
+                    .tabItem {
+                        Image(systemName: "globe")
+                        Text("Home")
+                    }
+                Text("PROFILE TAB")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Profile")
+                    }
+
         }
+        .accentColor(.red)
     }
 }
 
 #Preview {
     TabViewBootcamp()
+}
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.red.ignoresSafeArea(edges: .top)
+            Text("HOME TAB")
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+        }
+    }
 }
