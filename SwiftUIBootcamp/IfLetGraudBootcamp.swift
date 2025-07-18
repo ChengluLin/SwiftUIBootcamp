276e699f6a6923e21ac55f9aab0c54aaa8befe5e
Kevin <chenglu7701@gmail.com>
2025-07-18 13:32:04 +0800

    IfLetGraudBootcamp

---
+++ SwiftUIBootcamp/IfLetGraudBootcamp.swift
@@ -0,0 +1,55 @@
+//
+//  IfLetGraudBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by ChengLu on 2025/7/8.
+//
+
+import SwiftUI
+
+struct IfLetGraudBootcamp: View {
+    
+    @State var currentUserID: String? = "123"
+    @State var displyText: String? = nil
+    @State var isLoading: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Here we are practicing safe coding!")
+                if let text = displyText {
+                    Text(text)
+                        .font(.title)
+                }
+                if isLoading {
+                    ProgressView()
+                }
+                
+                Spacer()
+            }
+            .navigationTitle("Safe Coding")
+            .onAppear {
+                loadData()
+            }
+        }
+    }
+    
+    func loadData() {
+        
+        if let userID = currentUserID {
+            isLoading = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                displyText = "This is the new data!: \(userID)"
+                isLoading = false
+            }
+        } else {
+            displyText = "Error. There is no User ID!"
+        }
+        
+
+    }
+}
+
+#Preview {
+    IfLetGraudBootcamp()
+}
