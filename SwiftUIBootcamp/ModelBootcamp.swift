37c0a4810e7f128fb220968fa5178de2b33b84fe
Kevin <chenglu7701@gmail.com>
2025-08-12 11:56:04 +0800

    ModelBootcamp finish

--- SwiftUIBootcamp/ModelBootcamp.swift
+++ SwiftUIBootcamp/ModelBootcamp.swift
@@ -12,15 +12,16 @@ struct UserModel: Identifiable {
     let displayName: String
     let userName: String
     let followerCount: Int
+    let isVerified: Bool
 }
 
 struct ModelBootcamp: View {
     
     @State var users: [UserModel] = [
-        UserModel(displayName: "Nick", userName: "Nick123", followerCount: 100),
-        UserModel(displayName: "Emily", userName: "itsemily1995", followerCount: 55),
-        UserModel(displayName: "Samantha", userName: "ninja", followerCount: 355),
-        UserModel(displayName: "Chris", userName: "chris2009", followerCount: 88),
+        UserModel(displayName: "Nick", userName: "Nick123", followerCount: 100, isVerified: true),
+        UserModel(displayName: "Emily", userName: "itsemily1995", followerCount: 55, isVerified: true),
+        UserModel(displayName: "Samantha", userName: "ninja", followerCount: 355, isVerified: false),
+        UserModel(displayName: "Chris", userName: "chris2009", followerCount: 88, isVerified: true),
     ]
     
     var body: some View {
@@ -38,8 +39,18 @@ struct ModelBootcamp: View {
                                 .font(.caption)
                         }
                         Spacer()
+                        
+                        if user.isVerified {
+                            Image(systemName: "checkmark.seal.fill")
+                                .foregroundStyle(.blue)
+                        }
+                        
                         VStack {
-                            
+                            Text("\(user.followerCount)")
+                                .font(.headline)
+                            Text("Followers")
+                                .foregroundStyle(.gray)
+                                .font(.caption)
                         }
                     }
                     .padding(.vertical, 10)
