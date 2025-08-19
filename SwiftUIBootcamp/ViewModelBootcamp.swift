e1112c27f569d3d29644aaaa373345b9d411effe
Kevin <chenglu7701@gmail.com>
2025-08-19 13:30:51 +0800

    ViewModelBootcamp

---
+++ SwiftUIBootcamp/ViewModelBootcamp.swift
@@ -0,0 +1,67 @@
+//
+//  ViewModelBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by ChengLu on 2025/8/12.
+//
+
+import SwiftUI
+
+struct FruitModel: Identifiable {
+    let id: String = UUID().uuidString
+    let name: String
+    let count: Int
+}
+
+class FruitViewModel {
+    var fruitArray: [FruitModel] = []
+}
+
+struct ViewModelBootcamp: View {
+    
+    @State var fruitArray: [FruitModel] = [
+        FruitModel(name: "Apples", count: 5)
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(fruitArray) { fruit in
+                    HStack {
+                        Text("\(fruit.count)")
+                            .foregroundStyle(.red)
+                        
+                        Text(fruit.name)
+                            .font(.headline)
+                            .bold()
+                    }
+                }
+                .onDelete(perform: deleteFruit)
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Fruit List")
+            .onAppear {
+                getFruits()
+            }
+        }
+    }
+    
+    func getFruits() {
+        let fruit1 = FruitModel(name: "Orange", count: 1)
+        let fruit2 = FruitModel(name: "Banana", count: 2)
+        let fruit3 = FruitModel(name: "Watermelon", count: 88)
+        
+        fruitArray.append(fruit1)
+        fruitArray.append(fruit2)
+        fruitArray.append(fruit3)
+
+    }
+    
+    func deleteFruit(index: IndexSet) {
+        fruitArray.remove(atOffsets: index)
+    }
+}
+
+#Preview {
+    ViewModelBootcamp()
+}
