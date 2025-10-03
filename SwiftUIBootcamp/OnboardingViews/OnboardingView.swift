9c054e0b87ebd5d968fd3fff11bbc13b3146d1b7
Kevin <chenglu7701@gmail.com>
2025-10-03 13:33:40 +0800

    extension OnboardingView

--- SwiftUIBootcamp/OnboardingViews/OnboardingView.swift
+++ SwiftUIBootcamp/OnboardingViews/OnboardingView.swift
@@ -17,13 +17,26 @@ struct OnboardingView: View {
      3 - Add gender
      */
     
-    @State var onboardingState: Int = 0
+    @State var onboardingState: Int = 1
+    @State var name: String = ""
     
     var body: some View {
         ZStack {
             // content
             Color.purple
 //                .ignoresSafeArea()
+            ZStack {
+                switch onboardingState {
+                case 0:
+                    welcomeSection
+                case 1:
+                    addNameSection
+                default:
+                    RoundedRectangle(cornerRadius: 25)
+                        .foregroundStyle(.green)
+                }
+            }
+            
             // buttons
             VStack {
                 Spacer()
@@ -33,6 +46,16 @@ struct OnboardingView: View {
             .padding(30)
         }
     }
+}
+
+#Preview {
+    OnboardingView()
+//        .background(Color.purple)
+}
+
+// MARK: COMPONENTS
+
+extension OnboardingView {
     
     private var bottomButton: some View {
         Text("Sign in")
@@ -47,15 +70,72 @@ struct OnboardingView: View {
                        // 按鈕點擊後的動作
                    }
     }
-}
-
-#Preview {
-    OnboardingView()
-//        .background(Color.purple)
-}
-
-// MARK: COMPONENTS
-
-extension OnboardingView {
     
+    private var welcomeSection: some View {
+        VStack(spacing: 40) {
+            Spacer()
+            Image(systemName: "heart.text.square.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .foregroundStyle(.white)
+            Text("Find your match")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+                .overlay(
+                    Capsule(style: .continuous)
+                        .frame(height: 3)
+                        .offset(y: 5)
+                        .foregroundStyle(.white)
+                    , alignment: .bottom
+                )
+            Text("This the #1 app for finding your match online! In this tutorial we are practicing using AppStorage and other SwiftUI techniques.")
+                .fontWeight(.medium)
+                .foregroundStyle(.white)
+            Spacer()
+            Spacer()
+        }
+        .multilineTextAlignment(.center)
+        .padding(30)
+    }
+    
+    
+    private var addNameSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            Text("What.s your name?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+            TextField("Your name here...", text: $name)
+                .font(.headline)
+                .frame(height: 55)
+                .padding(.horizontal)
+                .background(.white)
+                .cornerRadius(10)
+            Spacer()
+            Spacer()
+        }
+        .padding(30)
+    }
+    
+    private var addAgeSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            Text("What.s your name?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+            TextField("Your name here...", text: $name)
+                .font(.headline)
+                .frame(height: 55)
+                .padding(.horizontal)
+                .background(.white)
+                .cornerRadius(10)
+            Spacer()
+            Spacer()
+        }
+        .padding(30)
+    }
 }
