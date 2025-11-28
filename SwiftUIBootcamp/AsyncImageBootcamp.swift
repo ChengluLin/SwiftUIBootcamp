f180460a2ce056e17c109cebcf2926bc8873acdb
Kevin <chenglu7701@gmail.com>
2025-11-28 14:12:16 +0800

    AsyncImageBootcamp

---
+++ SwiftUIBootcamp/AsyncImageBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncImageBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by ChengLu on 2025/11/25.
+//
+
+import SwiftUI
+
+struct AsyncImageBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncImageBootcamp()
+}
