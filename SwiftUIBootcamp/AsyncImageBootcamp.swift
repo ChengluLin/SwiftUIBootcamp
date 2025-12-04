ca3d32845e28dd8c092590db9ec6ceab466d3ad7
Kevin <chenglu7701@gmail.com>
2025-12-04 14:45:41 +0800

    AsyncImageBootcamp finish

--- SwiftUIBootcamp/AsyncImageBootcamp.swift
+++ SwiftUIBootcamp/AsyncImageBootcamp.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct AsyncImageBootcamp: View {
+    
+    let url = URL(string: "https://picsum.photos/200")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //        AsyncImage(url: url, content: { returnedImage in
+        //            returnedImage
+        //                .resizable()
+        //                .scaledToFit()
+        //                .frame(width: 200, height: 200)
+        //                .cornerRadius(20)
+        //        }, placeholder: {
+        //            ProgressView()
+        //        })
+        //            .frame(width: 100, height: 100)
+        
+        AsyncImage(url: url) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+            case .success(let returnedImage):
+                returnedImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(20)
+            case .failure(let error):
+                Image(systemName: "questionmark")
+                    .font(.headline)
+            default:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+            }
+        }
     }
 }
 
