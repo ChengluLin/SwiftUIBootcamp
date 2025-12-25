113fcb9e06d89506c9b1bc46aac5ece5b86f5478
Kevin <chenglu7701@gmail.com>
2025-12-25 13:35:47 +0800

    TextSelectionBootcamp

---
+++ SwiftUIBootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by ChengLu on 2025/12/17.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
