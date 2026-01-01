98012969daa6738b586964ea3b4617adaaba58ee
Kevin <chenglu7701@gmail.com>
2026-01-01 13:50:38 +0800

    ButtonStylesBootcamp

---
+++ SwiftUIBootcamp/ButtonStylesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonStylesBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by ChengLu on 2026/1/1.
+//
+
+import SwiftUI
+
+struct ButtonStylesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonStylesBootcamp()
+}
