842ece31e47271085db304e5d6b6407bd465b0eb
Kevin <chenglu7701@gmail.com>
2026-01-10 17:23:17 +0800

    ButtonStylesBootcamp

--- SwiftUIBootcamp/ButtonStylesBootcamp.swift
+++ SwiftUIBootcamp/ButtonStylesBootcamp.swift
@@ -9,7 +9,48 @@ import SwiftUI
 
 struct ButtonStylesBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            // 可改變可顯示框的大小＆可觸及範圍
+            Button {
+                print("action")
+            } label: {
+                Text("Button Title")
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 15))
+            .controlSize(.large)
+            
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.regular)
+            
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.mini)
+            
+            
+            Button("Button Title") {
+                
+            }
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.small)
+            
+        }
     }
 }
 
