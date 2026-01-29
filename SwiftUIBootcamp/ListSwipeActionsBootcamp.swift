6c93798f01cc4385485c4d8e3df0e940c3b06dc2
Kevin <chenglu7701@gmail.com>
2026-01-29 14:24:25 +0800

    ListSwipeActionsBootcamp

---
+++ SwiftUIBootcamp/ListSwipeActionsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ListSwipeActionsBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by ChengLu on 2026/1/29.
+//
+
+import SwiftUI
+
+struct ListSwipeActionsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListSwipeActionsBootcamp()
+}
